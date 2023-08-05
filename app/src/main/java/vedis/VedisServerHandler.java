6cb57770db3d1fc8e9f0e7cfdf5e05a26fd20d17
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-08-06 01:04:03 +0900

    Reformat

--- app/src/main/java/vedis/VedisServerHandler.java
+++ app/src/main/java/vedis/VedisServerHandler.java
@@ -1,15 +1,13 @@
 package vedis;
 
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.redis.*;
+import io.netty.util.ReferenceCountUtil;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
@@ -26,135 +24,150 @@ public class VedisServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (!(msg instanceof final ArrayRedisMessage req)) {
-            rejectMalformedRequest(ctx);
-            return;
-        }
-
-        final List<RedisMessage> args = req.children();
-        if (args.stream().anyMatch((arg) -> !(arg instanceof FullBulkStringRedisMessage))) {
-            rejectMalformedRequest(ctx);
-            return;
-        }
-
-        // For simplicity, convert all arguments into strings
-        // In production, handle them with byte[]
-        final List<String> strArgs =
-                args.stream()
-                    .map(FullBulkStringRedisMessage.class::cast)
-                    .map(bulkStr -> {
-                        if (bulkStr.isNull()) {
-                            return null;
-                        } else {
-                            return bulkStr.content().toString(StandardCharsets.UTF_8);
-                        }
-                    })
-                    .toList();
-
-        final String command = strArgs.get(0);
-        System.err.println(ctx.channel() + " RCVD: " + strArgs);
-
-        switch (command) {
-            case "COMMAND":  // dummy response
-                ctx.writeAndFlush(ArrayRedisMessage.EMPTY_INSTANCE);
-                break;
-            case "GET": {
-                if (strArgs.size() < 2) {
-                    reject(ctx, "ERR GET command requires a key argument");
-                    return;
-                }
-                final String key = strArgs.get(1);
-                if (key == null) {
-                    rejectNilKey(ctx);
-                    return;
-                }
-
-                final String value = map.get(key);
-                final FullBulkStringRedisMessage reply;
-                if (value != null) {
-                    reply = new FullBulkStringRedisMessage(
-                            Unpooled.copiedBuffer(value, StandardCharsets.UTF_8));
-                } else {
-                    reply = FullBulkStringRedisMessage.NULL_INSTANCE;
-                }
-                ctx.writeAndFlush(reply);
-                break;
+        try {
+            if (!(msg instanceof final ArrayRedisMessage req)) {
+                rejectMalformedRequest(ctx);
+                return;
             }
-            case "SET": {
-                if (strArgs.size() < 3) {
-                    reject(ctx, "ERR SET command requires a key, value argument");
-                    return;
-                }
-                final String key = strArgs.get(1);
-                if (key == null) {
-                    rejectNilKey(ctx);
-                    return;
-                }
-                final String value = strArgs.get(2);
-                if (value == null) {
-                    rejectNilValue(ctx);
-                    return;
-                }
 
-                // naive version
-                final boolean shouldReplyOldValue = strArgs.size() > 3 && "GET".equals(strArgs.get(3));
-                final String oldValue = map.put(key, value);
-                final RedisMessage reply;
-                if (shouldReplyOldValue) {
-                    if (oldValue != null) {
-                        reply = new FullBulkStringRedisMessage(
-                                Unpooled.copiedBuffer(oldValue, StandardCharsets.UTF_8));
-                    } else {
-                        reply = FullBulkStringRedisMessage.NULL_INSTANCE;
-                    }
-                } else {
-                    reply = new SimpleStringRedisMessage("OK");
-                }
-
-                ctx.writeAndFlush(reply);
-                break;
+            final List<RedisMessage> args = req.children();
+            if (args.stream().anyMatch((arg) -> !(arg instanceof FullBulkStringRedisMessage))) {
+                rejectMalformedRequest(ctx);
+                return;
             }
-            case "DEL": {
-                if (strArgs.size() < 2) {
-                    reject(ctx, "ERR DEL command requires at least one key argument");
-                    return;
-                }
 
-                int removedEntries = 0;
-                for (int i = 1; i < strArgs.size(); i++) {
-                    final String key = strArgs.get(i);
-                    if (key == null) {
-                        continue;
-                    }
-                    if (map.remove(key) != null) {
-                        removedEntries++;
-                    }
-                }
+            // For simplicity, convert all arguments into strings
+            // In production, handle them with byte[]
+            final List<String> strArgs =
+                    args.stream()
+                        .map(FullBulkStringRedisMessage.class::cast)
+                        .map(bulkStr -> {
+                            if (!bulkStr.isNull()) {
+                                return bulkStr.content().toString(StandardCharsets.UTF_8);
+                            } else {
+                                return null;
+                            }
+                        })
+                        .toList();
 
-                ctx.writeAndFlush(new IntegerRedisMessage(removedEntries));
-                break;
+            final String command = strArgs.get(0);
+            System.err.println(ctx.channel() + " RCVD: " + strArgs);
+
+            switch (command) {
+                case "COMMAND":  // dummy response
+                    ctx.writeAndFlush(ArrayRedisMessage.EMPTY_INSTANCE);
+                    break;
+                case "GET":
+                    handleGet(ctx, strArgs);
+                    break;
+                case "SET":
+                    handleSet(ctx, strArgs);
+                    break;
+                case "DEL":
+                    handleDel(ctx, strArgs);
+                    break;
+                case "SHUTDOWN":
+                    ctx.writeAndFlush(new SimpleStringRedisMessage("OK"))
+                       .addListener((ChannelFutureListener) f -> shutdownLatch.countDown());
+                    break;
+                default:
+                    reject(ctx, "ERR Unsupported command");
             }
-            case "SHUTDOWN":
-                ctx.writeAndFlush(new SimpleStringRedisMessage("OK"))
-                   .addListener((ChannelFutureListener) f -> {
-                       f.channel().close();
-                       shutdownLatch.countDown();
-                   });
-            default:
-                reject(ctx, "ERR Unsupported command");
+        } finally {
+            ReferenceCountUtil.release(msg);
         }
     }
 
-    private static void rejectNilValue(final ChannelHandlerContext ctx) {
-        reject(ctx, "ERR A nil value is not allowed");
+    private void handleGet(final ChannelHandlerContext ctx, final List<String> strArgs) {
+        if (strArgs.size() < 2) {
+            reject(ctx, "ERR GET command requires a key argument");
+            return;
+        }
+
+        final String key = strArgs.get(1);
+        if (key == null) {
+            rejectNilKey(ctx);
+            return;
+        }
+
+        final String value = map.get(key);
+        final FullBulkStringRedisMessage reply;
+        if (value != null) {
+            reply = newBulkStringMessage(value);
+        } else {
+            reply = FullBulkStringRedisMessage.NULL_INSTANCE;
+        }
+        ctx.writeAndFlush(reply);
+    }
+
+    private void handleSet(final ChannelHandlerContext ctx, final List<String> strArgs) {
+        if (strArgs.size() < 3) {
+            reject(ctx, "ERR SET command requires a key, value argument");
+            return;
+        }
+        final String key = strArgs.get(1);
+        if (key == null) {
+            rejectNilKey(ctx);
+            return;
+        }
+        final String value = strArgs.get(2);
+        if (value == null) {
+            rejectNilValue(ctx);
+            return;
+        }
+
+        // naive version
+        final boolean shouldReplyOldValue = strArgs.size() > 3 && "GET".equals(strArgs.get(3));
+        final String oldValue = map.put(key, value);
+        final RedisMessage reply;
+        if (shouldReplyOldValue) {
+            if (oldValue != null) {
+                reply = newBulkStringMessage(oldValue);
+            } else {
+                reply = FullBulkStringRedisMessage.NULL_INSTANCE;
+            }
+        } else {
+            reply = new SimpleStringRedisMessage("OK");
+        }
+
+        ctx.writeAndFlush(reply);
+    }
+
+    private void handleDel(final ChannelHandlerContext ctx, final List<String> strArgs) {
+        if (strArgs.size() < 2) {
+            reject(ctx, "ERR DEL command requires at least one key argument");
+            return;
+        }
+
+        int removedEntries = 0;
+        for (int i = 1; i < strArgs.size(); i++) {
+            final String key = strArgs.get(i);
+            if (key == null) {
+                continue;
+            }
+            if (map.remove(key) != null) {
+                removedEntries++;
+            }
+        }
+
+        ctx.writeAndFlush(new IntegerRedisMessage(removedEntries));
+    }
+
+    private static FullBulkStringRedisMessage newBulkStringMessage(final String value) {
+        return new FullBulkStringRedisMessage(
+                Unpooled.copiedBuffer(value, StandardCharsets.UTF_8));
+    }
+
+    private static void rejectMalformedRequest(final ChannelHandlerContext ctx) {
+        reject(ctx, "ERR Client request must be an array of bulk strings");
     }
 
     private static void rejectNilKey(final ChannelHandlerContext ctx) {
         reject(ctx, "ERR A nil key is not allowed");
     }
 
-    private void rejectMalformedRequest(final ChannelHandlerContext ctx) {
-        reject(ctx, "ERR Client request must be an array of bulk strings");
+    private static void rejectNilValue(final ChannelHandlerContext ctx) {
+        reject(ctx, "ERR A nil value is not allowed");
     }
 
     private static void reject(ChannelHandlerContext ctx, String error) {
